2c424e51e3576ad3fe1c0d9f8837cccd5a042fbc
Manuel <manudzmz@gmail.com>
2016-07-09 01:29:03 +0200

    Cambiado logo CR7 por copyright. Añadido mensaje de aviso al superar 150 palabras en el formulario. Añadido .js para el texto de los escudos.

--- js/form.js
+++ js/form.js
@@ -1,9 +1,10 @@
 var seleccion = document.getElementById('como')
 var infoOtros = document.getElementById('info-otros');
 var areaTexto = document.getElementById('zona-texto');
+var avisoPalabras = document.getElementById('aviso');
 
 
-seleccion.addEventListener('change', function(e){
+seleccion.addEventListener('change', function(){
 	if (this.value == 'Otros') {
 		infoOtros.style.display = 'block';
 	} else {
@@ -14,8 +15,9 @@ seleccion.addEventListener('change', function(e){
 areaTexto.addEventListener('change', function(){
 	var wordLen = 150;
 	var len = this.value.split(/[\s]+/); 
-    if(len.length -1 > wordLen){
-    	alert("No está permitido escribir más de "+wordLen+" palabras.");
-        return false;
+    if(len.length > wordLen){
+    	avisoPalabras.style.display = 'block';
+    } else {
+    	avisoPalabras.style.display = 'none';
     }
 });
